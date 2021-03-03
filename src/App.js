910a86e9e162d3e38b19179742790784d2fa321c
rjsanx <supratiksantra67@gmail.com>
2021-03-04 01:51:19 +0530

    deploy react counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      show: true
+    };
+  }
+
+
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  }
+  DecrementItem = () => {
+    this.setState({ clicks: this.state.clicks - 1 });
+  }
+  ToggleClick = () => {
+    this.setState({ show: !this.state.show });
+  }
+
+  render() {
+    const { clicks, show, view } = this.state
+
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+        <div>
+          <h1
+            style={{
+              color: 'white',
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}>Counter</h1>
+          <button onClick={this.IncrementItem}>Add + 1</button>
+          <button onClick={this.DecrementItem}>Sub - 1</button>
+          <h1 style={{
+            color: 'white',
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}>{show ? clicks : view}</h1>
+          <footer
+            style={{
+              color: 'white',
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}>by @rjsanx</footer>
+        </div>
+
+
+
+      </div>
+
+    )
+  }
 }
-
-export default App;
